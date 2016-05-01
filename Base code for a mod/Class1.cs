3693b3f740205db5ed83c465156084ed7bc09a81
MaxTCC <official.crazycookiemax@gmail.com>
2016-05-01 17:25:41 +0100

    Added files via upload

---
+++ Base code for a mod/Class1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Mod
+{
+    public class Main : MonoBehaviour
+    {
+        public void Start()
+        {
+            var modObject = new GameObject();
+            modObject.AddComponent<modComponent>();
+            DontDestroyOnLoad(modObject);
+        }
+    }
+
+
+    public class modComponent : MonoBehaviour
+    {
+        //This is where you do the code and all that jazz.
+    }
+}
